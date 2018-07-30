4053945cb377b0ad5c19c65305da95708310efc9
DhruvUpadhyaya <40164639+DhruvUpadhyaya@users.noreply.github.com>
2018-07-31 03:45:54 +0530

    Login Done !!

--- app/src/main/java/com/example/dhruvupadhyaya/snapchatclone/LoginActivity.java
+++ app/src/main/java/com/example/dhruvupadhyaya/snapchatclone/LoginActivity.java
@@ -1,13 +1,87 @@
 package com.example.dhruvupadhyaya.snapchatclone;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText mEmail;
+    private EditText mPassword;
+    private Button mLogin;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null){
+                    Intent intent1 = new Intent(getApplication(),MainActivity.class);
+                    intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent1);
+                    finish();
+                    return;
+                }
+            }
+        };
+
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+
+        mLogin = findViewById(R.id.loginLoginActivity);
+        mAuth = FirebaseAuth.getInstance();
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email = mEmail.getText().toString();
+                final String password = mPassword.getText().toString();
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener((Executor) getApplication(),
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful()){
+                                    Toast.makeText(LoginActivity.this, "Sign in ERROR", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                        });
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(firebaseAuthStateListener);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(firebaseAuthStateListener);
     }
 }
