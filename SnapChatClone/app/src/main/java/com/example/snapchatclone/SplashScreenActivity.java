3b08633123ea62c1d92321fe89fe54cbb2894916
DhruvUpadhyaya <2793dhruv@gmail.com>
2020-06-27 17:55:16 +0530

    Resgistration and Login

--- SnapChatClone/app/src/main/java/com/example/snapchatclone/SplashScreenActivity.java
+++ SnapChatClone/app/src/main/java/com/example/snapchatclone/SplashScreenActivity.java
@@ -1,4 +1,36 @@
 package com.example.snapchatclone;
 
-public class SplashScreenActivity {
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    public static boolean started = false;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null){
+            Intent intent = new Intent(getApplication(), MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+            return;
+        }else{
+            Intent intent = new Intent(getApplication(), ChooseLoginRegistrationActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+            return;
+        }
+    }
 }
